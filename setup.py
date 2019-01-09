ea08e59395cbcd802c1209e222d16c27468839d3
Joseph Mancuso <idmann509@gmail.com>
2019-01-08 23:03:54 -0500

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="masonite-scheduler",
-    version='1.0.0',
+    version='1.0.1',
     packages=[
         'scheduler',
         'scheduler.commands',
