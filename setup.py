4d9afb88f505825680d6d846de08215d43875b8c
Joseph Mancuso <idmann509@gmail.com>
2019-11-29 13:55:26 -0500

    added package data

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup
 setup(
     name="masonite-scheduler",
     version='1.0.2',
+    package_dir={'': 'src'},
     packages=[
         'scheduler',
         'scheduler.commands',
         'scheduler.providers'
     ],
+    author='Joe Mancuso',
+    author_email='joe@masoniteproject.com',
     install_requires=[],
     include_package_data=True,
 )
---
+++ tests/test_basic.py
@@ -0,0 +1,6 @@
+from masonite.testing import TestCase
+
+class TestBasic(TestCase):
+
+    def test_can_assert_true(self):
+        self.assertTrue(True)
