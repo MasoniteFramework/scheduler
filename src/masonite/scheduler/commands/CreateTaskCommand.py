29dd9fb221fef8f93eb0addce7c134299c3a60be
Joe Mancuso <idmann509@gmail.com>
2020-09-18 11:09:27 -0400

    linted

--- src/masonite/scheduler/commands/CreateTaskCommand.py
+++ src/masonite/scheduler/commands/CreateTaskCommand.py
@@ -21,13 +21,13 @@ class CreateTaskCommand(Command):
 
             f = open("app/tasks/{0}.py".format(task), "w+")
 
-            f.write("''' Task Module Description '''\n".format(task))
+            f.write("''' Task Module Description '''\n")
             f.write("from masonite.scheduler.Task import Task\n\n")
             f.write(
                 "class {0}(Task):\n    ''' Task description '''\n\n    ".format(task)
             )
-            f.write("def __init__(self):\n        pass\n\n    ".format(task))
-            f.write("def handle(self):\n        pass\n".format(task))
+            f.write("def __init__(self):\n        pass\n\n    ")
+            f.write("def handle(self):\n        pass\n")
 
             self.info("Task Created Successfully!")
         else:
